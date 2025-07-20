87d6bab14028b4b117fd34a820b898b6d03169a0
John Rodrigues <43817570+RodriguesJohn@users.noreply.github.com>
2025-07-20 15:11:29 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".my-button").onclick = function () {
-  var card = document.getElementById("card1");
-  card.style.display = "block";
-  // Force reflow to ensure the transition runs
-  void card.offsetWidth;
-  card.classList.add("show");
-};
